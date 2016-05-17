5767d191e2565998abba1a601de875f2852b81dc
Scott Wales <scott.wales@unimelb.edu.au>
2016-05-17 12:30:35 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# cylcutil
+
+[![Documentation Status](https://readthedocs.org/projects/cylcutil/badge/?version=latest)](https://readthedocs.org/projects/cylcutil/?badge=latest)
+[![Build Status](https://travis-ci.org/ScottWales/cylcutil.svg?branch=master)](https://travis-ci.org/ScottWales/cylcutil)
+[![codecov.io](http://codecov.io/github/ScottWales/cylcutil/coverage.svg?branch=master)](http://codecov.io/github/ScottWales/cylcutil?branch=master)
+[![Code Health](https://landscape.io/github/ScottWales/cylcutil/master/landscape.svg?style=flat)](https://landscape.io/github/ScottWales/cylcutil/master)
+[![Code Climate](https://codeclimate.com/github/ScottWales/cylcutil/badges/gpa.svg)](https://codeclimate.com/github/ScottWales/cylcutil)
+[![PyPI version](https://badge.fury.io/py/cylcutil.svg)](https://pypi.python.org/pypi/cylcutil)
+
+Build Instructions:
+-------------------
+
+    mkdir build && pushd build
+    cmake ..
+    make
+    ctest
---
+++ setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#
+# Copyright 2015 ARC Centre of Excellence for Climate Systems Science
+# 
+# author: Scott Wales <scott.wales@unimelb.edu.au>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup, find_packages
+from subprocess import Popen, PIPE
+
+def git_describe():
+    proc = Popen(['git','describe','--always','--long'],stdout=PIPE)
+    stdout, stderr  = proc.communicate()
+    if proc.returncode != 0:
+        raise Exception('git error: %s'%stderr)
+    desc    = stdout[0].decode('utf-8').strip()
+    try:
+        ihash   = desc.rindex('-')
+    except ValueError:
+        raise Exception('No git tag available for version')
+    ichange = desc.rindex('-',0,ihash)
+    return (desc[0:ichange],desc[ichange+1:ihash],desc[ihash+1:-1])
+
+def pep440():
+    tag, changes, hash = git_describe()
+    if changes == 0:
+        return tag
+    else:
+        return "%s.dev%s+%s"%(tag,changes,hash)
+
+release = pep440()
+
+requirements = [
+        'six',
+        ]
+
+setup(
+        name             = 'cylcutil',
+        version          = release,
+        url              = 'https://github.com/ScottWales/cylcutil',
+        packages         = find_packages(exclude=['tests*']),
+        install_requires = requirements,
+
+        author           = 'Scott Wales',
+        author_email     = 'scott.wales@unimelb.edu.au',
+        description      = 'Utilities for working with Cylc Suites',
+        license          = 'Apache 2.0',
+        )
